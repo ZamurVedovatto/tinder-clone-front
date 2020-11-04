55783ed61ddf6e15476ca0fab62756febbae77f9
Zamur Vedovatto <vedovatto@gmail.com>
2020-11-03 21:05:23 -0300

    first

---
+++ src/Components/SwipeButtons.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'SwipeButtons.scss'
+
+export default function SwipeButtons() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/Components/TinderCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TinderCards.scss'
+
+export default function TinderCards() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
